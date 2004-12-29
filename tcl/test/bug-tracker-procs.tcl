1380230f4e63be53ce2f1aa417cc1f2eb7db35ca
Don Baccus <dhogaza@pacifier.com>
2004-12-29 02:23:45 +0000

    Finished up some more basic testing

--- tcl/test/bug-tracker-procs.tcl
+++ tcl/test/bug-tracker-procs.tcl
@@ -22,6 +22,8 @@ aa_register_case -cats {api smoke} project_new {
                 # does not work.
                 set old_package_id [ad_conn package_id]
                 ad_conn -set package_id $site_node(package_id)
+                set package_id [ad_conn package_id]
+                set user_id [ad_conn user_id]
 
                 array set default_configs [bug_tracker::get_default_configurations]
                 if { ![info exists default_configs(Bug-Tracker)] } {
@@ -33,6 +35,23 @@ aa_register_case -cats {api smoke} project_new {
                     bug_tracker::install_parameters_setup -spec $config(parameters)
                     aa_equals "Bug tracker project creation test" [db_string count_projects {}] 1
                 }
+
+                # Create a dummy component
+                db_1row new_component_id {}
+                db_dml new_component {}
+
+                db_1row new_bug_id {}
+                bug_tracker::bug::new \
+                    -bug_id $bug_id \
+                    -package_id $package_id \
+	            -component_id $component_id \
+	            -found_in_version [bug_tracker::conn user_version_id] \
+	            -summary summary \
+	            -description description \
+	            -desc_format text/html \
+                    -keyword_ids {}
+                aa_log_result pass "Successfully created a bug"
+
                 ad_conn -set package_id $old_package_id
             }
         }
