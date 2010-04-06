f55da033db762c24f6df9f8d4881b22840a02e13
Ryan Gallimore <ryan.gallimore@quest.ie>
2010-04-06 02:01:05 +0000

    Fixed test to work with any bug tracker URL.

--- tcl/test/bug-tracker-procs.tcl
+++ tcl/test/bug-tracker-procs.tcl
@@ -12,8 +12,9 @@ aa_register_case -cats {api smoke} project_new {
     aa_run_with_teardown \
         -rollback \
         -test_code {
-            if { [catch {array set site_node [site_node::get -url /bug-tracker]} errmsg] } {
-                aa_error "Can't find bug-tracker at /bug-tracker: $errmsg"
+            set bt_package_id [lindex [apm_package_ids_from_key -package_key bug-tracker -mounted] 0]
+            if { $bt_package_id eq "" } {
+                aa_error "Can't find bug-tracker instance."
             } else {
 
                 # Don't believe the bug-tracker Tcl API that misleads you into
@@ -21,15 +22,16 @@ aa_register_case -cats {api smoke} project_new {
                 # various procs.  The vile bug_tracker::conn proc guarantees this
                 # does not work.
                 set old_package_id [ad_conn package_id]
-                ad_conn -set package_id $site_node(package_id)
+                ad_conn -set package_id $bt_package_id
                 set package_id [ad_conn package_id]
                 set user_id [ad_conn user_id]
 
                 array set default_configs [bug_tracker::get_default_configurations]
-                if { ![info exists default_configs(Bug-Tracker)] } {
-                    aa_error "Can't find default bug-tracker configuration"
+                
+                if { ![info exists "default_configs(Bug Tracker)"] } {
+                    aa_error "Can't find default bug-tracker configuration"                    
                 } else {
-                    array set config $default_configs(Bug-Tracker)
+                    array set config "$default_configs(Bug Tracker)"
                     bug_tracker::delete_all_project_keywords 
                     bug_tracker::install_keywords_setup -spec $config(categories)
                     bug_tracker::install_parameters_setup -spec $config(parameters)
