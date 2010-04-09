25250d9a0a577e658b4f7a87303830cd5c090f33
Ryan Gallimore <ryan.gallimore@quest.ie>
2010-04-09 21:36:33 +0000

    Fix workflow-edit link

--- www/admin/index.tcl
+++ www/admin/index.tcl
@@ -22,6 +22,8 @@ set context_bar [ad_context_bar]
 
 db_1row project_info { } -column_array project
 
+set workflow_id [bug_tracker::bug::get_instance_workflow_id]
+
 set project(maintainer_url) [acs_community_member_url -user_id $project(maintainer)]
 
 set project_edit_url "project-edit"
@@ -29,12 +31,13 @@ set project_maintainer_edit_url "project-edit"
 set versions_edit_url "versions"
 set categories_edit_url "categories"
 set permissions_edit_url "permissions"
-set workflow_edit_url "workflow-edit"
+set workflow_url [site_node::get_package_url -package_key workflow]
+set workflow_edit_url [export_vars -base "${workflow_url}admin/workflow-edit" {workflow_id}]
 set parameters_edit_url "/shared/parameters?[export_vars { { return_url [ad_return_url] } { package_id {[ad_conn package_id]} } }]"
 set severity_codes_edit_url "severity-codes"
 set priority_codes_edit_url "priority-codes"
 set workflow_pretty_name [workflow::get_element -element pretty_name \
-                             -workflow_id [bug_tracker::bug::get_instance_workflow_id]]
+                             -workflow_id $workflow_id]
 db_multirow -extend { edit_url delete_url maintainer_url view_bugs_url } components components {} {
     set edit_url "component-ae?[export_vars { component_id }]"
     if { $num_bugs == 0 } {
